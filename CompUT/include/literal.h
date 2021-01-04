73763ca63bef4f2ed4190f813341a0d89a8d63a6
Sosthène Leroy <leroysos@etu.utc.fr>
2021-01-04 20:28:04 +0100

    Literal fix

--- CompUT/include/literal.h
+++ CompUT/include/literal.h
@@ -18,9 +18,9 @@ public:
     static LiteralType isLiteral(const std::string& s);
     static const std::shared_ptr<Literal> makeLiteral(const std::string& s, LiteralType t);
 
-    virtual LiteralType getType() = 0;
+    virtual LiteralType getType() const = 0;
     virtual std::string toString() const = 0;
-    virtual std::shared_ptr<Literal> getCopy() { return makeLiteral(this->toString(), this->getType());}
+    virtual std::shared_ptr<Literal> getCopy() const { return makeLiteral(this->toString(), this->getType());}
     virtual ~Literal() {}
 };
 
@@ -28,7 +28,7 @@ class LInteger : public Literal {
 
     int value;
 public:
-    LiteralType getType(){ return linteger;}
+    LiteralType getType() const { return linteger;}
     LInteger(const int& i): value(i){}
     int getValue() const { return value;}
     const std::shared_ptr<LInteger> operator=(const std::shared_ptr<LInteger>){
@@ -36,20 +36,20 @@ public:
     }
     static const std::shared_ptr<LInteger> makeLiteral(const int& i);
     std::string toString() const { return std::to_string(value);}
-    ~LInteger(){}
+    ~LInteger() = default;
 };
 
 class LReal : public Literal {
 
     double value;
 public:
-    LiteralType getType(){ return lreal;}
+    LiteralType getType() const { return lreal;}
     LReal(const double& d): value(d){}
     LReal(const LInteger& i): value(double(i.getValue())){}
     double getValue() const { return value;}
     static const std::shared_ptr<LReal> makeLiteral(const double& d);
     std::string toString() const{ return std::to_string(value);}
-    ~LReal(){}
+    ~LReal() = default;
 };
 
 class LRational : public Literal {
@@ -58,7 +58,7 @@ class LRational : public Literal {
     int den;
     void simplify();
 public:
-    LiteralType getType(){ return lrational;}
+    LiteralType getType() const { return lrational;}
     LRational(const int& n, const int& d): num(n),den(d){if (den == 0) throw LiteralException("Null denominator"); this->simplify();}
     LRational(const LInteger& i): num(i.getValue()), den(1){}
     int getNum() const { return num;}
@@ -67,40 +67,30 @@ public:
     std::string toString() const { return std::to_string(num)+'/'+std::to_string(den);}
     LInteger toLInteger(){ LInteger i(int(num/den)); return i;}
     LReal toLReal(){ LReal r(double(num/den)); return r;}
-    ~LRational(){}
+    ~LRational() = default;
 };
 
-class LAtom : public Literal {
+class LExpression : public Literal {
 
     std::string value;
 public:
-    LiteralType getType(){ return latom;}
-    std::string getValue() const { return value;}
-    std::string toString() const {return value;}
-    ~LAtom(){}
-};
-
-class LExpression : public LAtom {
-
-    std::string value;
-public:
-    LiteralType getType(){ return lexpression;}
+    LiteralType getType() const { return lexpression;}
     std::string getValue() const { return value;}
     std::string toString() const { return '\''+value+'\'';}
     static const std::shared_ptr<LExpression>makeLiteral(std::string s);
     LExpression(std::string s) : value(s){}
-    ~LExpression(){}
+    ~LExpression() = default;
 };
 
 class LProgram : public Literal {
     std::string program;
 public:
-    LiteralType getType(){ return lprogram;}
+    LiteralType getType() const { return lprogram;}
     std::string toString() const;
     static const std::shared_ptr<LProgram>makeLiteral(std::string s);
     LProgram(std::string s) : program(s){}
     std::string getValue() {return program;}
-    ~LProgram(){}
+    ~LProgram() = default;
 };
 
 #endif
